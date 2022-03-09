19d5df41080de575c970f7c237e1eedd245c757c
Taylor Huffman <taylor@crucialartists.com>
2022-03-09 13:35:55 -0600

    Complete assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
-// create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+    const [showDogs, setShowDogs] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+  
+    useEffect(() => {
+      fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then((data) => {
+          setShowDogs(data.message);
+          setIsLoaded(true);
+        });
+    }, []);
+  
+    if (!isLoaded) return <p>Loading...</p>;
+  
+    return <img src={showDogs} alt="A Random Dog"></img>;
+  }
+
+export default App
